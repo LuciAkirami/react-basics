0127a336e6fc596aa4f60d9d410027b08a2e563e
LuciAkirami <110820999+LuciAkirami@users.noreply.github.com>
2024-08-07 16:00:18 +0000

    added random generator

--- 06useEffect/src/App.jsx
+++ 06useEffect/src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(2)
+
+  function randGenerator(length){
+    let final=0;
+    let num;
+
+    for(let i=1; i <= length; i++) {
+        num = Math.floor(Math.random() * 9 + 1) // 2 -> 2, 3*10 -> 32, 0*100 -> 32
+        num = num * (10**(i-1))
+        final += num
+
+    }
+    
+    console.log(final)
+    return final
+
+  }
+
+  let k = randGenerator(length)
+
 
   return (
     <>
       <h1>Henlo</h1>
+      <h2>{k}</h2>
+      <input type="range" min="1" max="10" defaultValue={length} onChange={(event) => setLength(event.target.value)}/>
+      <p>{length}</p>
     </>
   )
 }
