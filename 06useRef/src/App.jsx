5e2ca92e31f4205465bf85367e7c8aa0ed71c383
LuciAkirami <ajsubscriber001@gmail.com>
2024-08-20 19:53:00 +0530

    created useRef

---
+++ 06useRef/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-blue-400 text-lg'>Henlo</h1>
+    </>
+  )
+}
+
+export default App
+
+/**
+ * The ref points to a number, but, like state, you could point to anything: a string, an object, or even a function. Unlike state, ref is a plain JavaScript object with the current property that you can read and modify.
+
+Note that the component doesn’t re-render with every increment. Like state, refs are retained by React between re-renders. However, setting state re-renders a component. Changing a ref does not!
+ * 
+ */
