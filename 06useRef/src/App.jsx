8a56b470dda611e71500616876dd2fb77f9a1208
LuciAkirami <ajsubscriber001@gmail.com>
2024-08-20 20:26:56 +0530

    updated useRef

--- 06useRef/src/App.jsx
+++ 06useRef/src/App.jsx
@@ -3,7 +3,7 @@ import Component1 from "./components/RefinTimers";
 function App() {
   return (
     <>
-      <h1 className="text-blue-400 text-lg">Henlo</h1>
+      <h1 className="text-blue-400 text-lg mx-auto">Henlo</h1>
       <Component1 />
     </>
   );
@@ -19,5 +19,21 @@ Note that the component doesn’t re-render with every increment.
 Like state, refs are retained by React between re-renders. 
 However, setting state re-renders a component. Changing a ref does not!
 
-ref
+------ When to use Ref and When to use State --------
+- When a piece of information is used for rendering, keep it in state. 
+- When a piece of information is only needed by event handlers and changing it doesn’t require a re-render, 
+using a ref may be more efficient.
+
+------ Implementing useRef vis useState --------------
+// Inside of React
+function useRef(initialValue) {
+  const [ref, unused] = useState({ current: initialValue });
+  return ref;
+}
+
+During the first render, useRef returns { current: initialValue }. 
+This object is stored by React, so during the next render the same object will be returned. 
+Note how the state setter is unused in this example. 
+It is unnecessary because useRef always needs to return the same object!
+So its like a state variable without setter
 */
--- 06useRef/src/components/RefinTimers.jsx
+++ 06useRef/src/components/RefinTimers.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 
-/*
+/* ------------- Ref in setInterval / clearInterval ----------
 When the “Stop” button is pressed, you need to cancel the existing interval so that it stops 
 updating the now state variable. You can do this by calling clearInterval, but you need to give 
 it the interval ID that was previously returned by the setInterval call when the user pressed Start. 
@@ -8,10 +8,20 @@ You need to keep the interval ID somewhere. Since the interval ID is not used fo
 you can keep it in a ref
 */
 export default function Component1() {
+  // store the start time
   let [startTime, setStartTime] = useState(0);
+  // store the current time
   let [now, setNow] = useState(0);
 
   let intervalRef = useRef(null);
+
+  // store the count
+  let countRef = useRef(0);
+
+  function updateCount() {
+    // This doesn't re-render the component!
+    countRef.current = countRef.current + 1;
+  }
 
   function handleStart() {
     setStartTime(Date.now());
@@ -38,7 +48,7 @@ export default function Component1() {
 
   return (
     <>
-      <h1 className="mx-5">Time Passed: {secondsPassed.toFixed(3)}</h1>
+      <h2 className="mx-5">Time Passed: {secondsPassed.toFixed(3)}</h2>
       <button
         className="mx-4 bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded"
         onClick={handleStart}
@@ -50,6 +60,14 @@ export default function Component1() {
         onClick={handleStop}
       >
         stop
+      </button>
+      <h2 className="m-4">Count Value: {countRef.current}</h2>
+      {/* // This doesn't re-render the component! */}
+      <button
+        className="mx-5 bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded"
+        onClick={updateCount}
+      >
+        Increase Count
       </button>
     </>
   );
